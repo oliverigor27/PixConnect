18b3bc4039932c09e23df6df323342d1072e1ffa
Igor Oliveira <igoroliveira9827@gmail.com>
2024-04-26 20:36:04 -0300

    feat: create a new payment dto

--- src/Modules/Payment/Dto/payment.dto.ts
+++ src/Modules/Payment/Dto/payment.dto.ts
@@ -1,4 +1,10 @@
 export class PaymentDto
 {
-    
+    Value: number;
+    Description: string;
+
+    constructor(value: number, description: string) {
+        this.Value = value;
+        this.Description = description;
+    }
 }
